03fdf6c787d6dcef472fa7be92a20bdd0f7567dc
Gosha_Krasilnikov <grgkrslnkv@gmail.com>
2023-10-18 22:48:48 +0300

    main: add app package

--- convert.py
+++ convert.py
@@ -1,16 +1,17 @@
 from os import listdir, remove, rename
 
-from PIL import Image
+from PIL import Image 
 from glob import glob
 
 
 def compress_img(dir1, dir2, quality=100, width=499, height=499):
-    for file in dir1:
+    for file in listdir(dir1):
         name = file
+        print(name)
         in_dir = listdir(dir1 + "/" + name)
         for file in in_dir:
-            img = Image.open("dir1" + "/" + name + "/" + file)
-            img = img.resize((width, height), Image.ANTIALIAS)
+            img = Image.open(dir1 + "/" + name + "/" + file)
+            img = img.resize((width, height))
             try:
                 img.save(dir2 + "/" + file, quality=quality, optimize=True)
             except OSError:
@@ -28,38 +29,28 @@ def convert_images(dir, name):
         i += 1
         rename(dir + "/" + file, dir + "/" + new_name)
 
+# perelimb_train_zdorov_dir = listdir("perelimb/train/zdorov")
+# pereperelimb_train_bolen_dir = listdir("perelimb/train/bolen")
+# perelimb_test_zdorov_dir = listdir("perelimb/test/zdorov")
+# perelimb_test_bolen_dir = listdir("perelimb/test/bolen")
 
-limb_train_zdorov_dir = listdir("limb/train/zdorov")
-limb_train_bolen_dir = listdir("limb/train/bolen")
-limb_test_zdorov_dir = listdir("limb/test/zdorov")
-limb_test_bolen_dir = listdir("limb/test/bolen")
+# limb_train_dir = glob("limb/train/**/*.bmp", recursive=True)
+# limb_test_dir = glob("limb/test/**/*.bmp", recursive=True)
+# perelimb_train_dir = glob("perelimb/train/**/*.bmp", recursive=True)
+# perelimb_test_dir = glob("perelimb/test/**/*.bmp", recursive=True)
 
-perelimb_train_zdorov_dir = listdir("perelimb/train/zdorov")
-pereperelimb_train_bolen_dir = listdir("perelimb/train/bolen")
-perelimb_test_zdorov_dir = listdir("perelimb/test/zdorov")
-perelimb_test_bolen_dir = listdir("perelimb/test/bolen")
+# compress_img("norma_limb", "limb/zdorov")
+# compress_img("nenorma_limb", "limb/bolen")
 
-limb_train_dir = glob("limb/train/**/*.bmp", recursive=True)
-limb_test_dir = glob("limb/test/**/*.bmp", recursive=True)
-perelimb_train_dir = glob("perelimb/train/**/*.bmp", recursive=True)
-perelimb_test_dir = glob("perelimb/test/**/*.bmp", recursive=True)
+# compress_img("norma_perelimb", "perelimb/zdorov")
+# compress_img("nenorma_perelimb", "perelimb/bolen")
 
-compress_img("limb_norma_train", limb_train_zdorov_dir)
-compress_img("limb_nenorma_train", limb_train_bolen_dir)
-compress_img("limb_norma_test", limb_zdorov_dir)
-compress_img("limb_nenorma_test", limb_limb_test_bolen_dir)
+convert_images("limb/zdorov", "zdorov.")
+convert_images("limb/bolen", "bolen.")
+convert_images("perelimb/zdorov", "zdorov.")
+convert_images("perelimb/bolen", "bolen.")
 
-compress_img("perelimb_norma_train", perelimb_train_zdorov_dir)
-compress_img("perelimb_nenorma_train", perelimb_train_bolen_dir)
-compress_img("perelimb_norma_test", perelimb_zdorov_dir)
-compress_img("perelimb_nenorma_test", perelimb_limb_test_bolen_dir)
-
-convert_images(limb_train_zdorov_dir, "zdorov.")
-convert_images(limb_train_bolen_dir, "bolen.")
-convert_images(limb_test_zdorov_dir, "zdorov.")
-convert_images(limb_test_bolen_dir, "bolen.")
-
-convert_images(perelimb_train_zdorov_dir, "zdorov.")
-convert_images(perelimb_train_bolen_dir, "bolen.")
-convert_images(perelimb_test_zdorov_dir, "zdorov.")
-convert_images(perelimb_test_bolen_dir, "bolen.")
+# convert_images(perelimb_train_zdorov_dir, "zdorov.")
+# convert_images(perelimb_train_bolen_dir, "bolen.")
+# convert_images(perelimb_test_zdorov_dir, "zdorov.")
+# convert_images(perelimb_test_bolen_dir, "bolen.")
--- limb/train11.py
+++ limb/train11.py
@@ -1,7 +1,7 @@
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential, load_model
 from keras.layers import Dense, Flatten, Dropout
-from keras.applications import InceptionV3
+from keras.applications import VGG19
 from keras.callbacks import ModelCheckpoint
 
 train_path = "train"  # путь для обучающей выборки
@@ -12,7 +12,7 @@ train_datagen = ImageDataGenerator(
 )
 test_datagen = ImageDataGenerator(rescale=1.0 / 255)
 
-batch_size = 15  # размерность подвыборки
+batch_size = 20  # размерность подвыборки
 img_width, img_height = (
     499,
     499,
@@ -31,41 +31,36 @@ test_generator = test_datagen.flow_from_directory(
     class_mode="binary",
 )  # генератор для тестирования на контрольной выборке
 
-# inception = InceptionV3(
-#     include_top=False, input_shape=(img_width, img_height, 3)
-# )  # импорт модели InceptionV3
-
-# for layer in inception.layers:
-#     layer.trainable = False  # игнорируем слой для классификации изображений на ILSVRC
-
-# model = (
-#     Sequential()
-# )  # немного модифицируем нашу модель, добавляя несколько дополнительных слоёв
-# model.add(inception)
-# model.add(Flatten())
-# model.add(Dense(256, activation="relu"))
-# model.add(Dropout(0.5))
-# model.add(Dense(1, activation="sigmoid"))
-model = load_model("perelimb_model.h5")
-model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
+resnet = VGG19(
+    include_top=False, input_shape=(img_width, img_height, 3), weights='imagenet'
+)  # импорт модели InceptionV3
+resnet.trainable = False  # игнорируем слой для классификации изображений на ILSVRC
 
 steps_per_epoch = (
     train_generator.n // train_generator.batch_size
 )  # устанавливаем размеры шагом
 validation_steps = test_generator.n // test_generator.batch_size
 callback = ModelCheckpoint(
-    filepath="perelimb_model1.h5", monitor="val_accuracy", save_best_only=True
+    filepath="limb_model1.h5", monitor="val_accuracy", save_best_only=True
 )
+model = (
+    Sequential()
+)  # немного модифицируем нашу модель, добавляя несколько дополнительных слоёв
+model.add(resnet)
+model.add(Flatten())
+model.add(Dense(128, activation="relu"))
+model.add(Dropout(0.5))
+model.add(Dense(1, activation="sigmoid"))
 
-
+model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
 model.fit(
     train_generator,
     epochs=15,
     steps_per_epoch=steps_per_epoch,
     validation_data=test_generator,
     validation_steps=validation_steps,
-    callbacks=callback,
-)
+    callbacks=[callback],
+)  # обучение модели
 
 accuracy = model.evaluate(test_generator)
 
